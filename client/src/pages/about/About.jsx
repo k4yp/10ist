fa02f698a494e0971ef0d023e2c243d598218b33
keyp0s <aidentubmann@gmail.com>
2022-03-27 23:20:08 +1100

    updated about page

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -3,21 +3,23 @@ import './about.css'
 export default function About() {
   return (
     <div className='about'>
-        <h1 id="pandex">pandex</h1>
-        <p>this was created for 10ist web design project</p>
-        <a href="https://github.com/keyp0s/pandex">link to github repo</a>
-        <h3 id="about-the-website">about the website</h3>
-        <p>pandex is a place to share screenshots and configurations of all your *nix desktops, themes or any other cool customisations</p>
-        <h3 id="technologies-used">technologies used</h3>
-        <p><strong>frontend</strong></p>
+        <h1 id="pandex">Pandex</h1>
+        <p>This was created for 10ist web design project  </p>
+        <h3 id="about-the-website">About the website</h3>
+        <p>Pandex is a place to share screenshots and configurations of all your *nix desktops, themes or any other customisations</p>
+        <h3 id="technologies-used">Technologies used</h3>
+        <p><strong>Frontend</strong>  </p>
         <ul>
-            <li>react: chosen javascript libary to create responsive and reactive websites   </li>
+        <li><a href="https://reactjs.org">React</a> <strong>:</strong> Chosen javascript libary to create responsive and reactive websites  </li>
         </ul>
-        <p><strong>backend (still under development)</strong></p>
+        <p><strong>Backend</strong></p>
         <ul>
-            <li>node.js: javascript runtime environment that allows js to run server side</li>
-            <li>express: back end web application framework for node.js</li>
-            <li>mongo db: document oriented NoSQL database</li>
+        <li><a href="https://nodejs.org/en/">Node.js</a> <strong>:</strong> A Runtime environment that allows JS to run server side</li>
+        <li><a href="https://expressjs.com/">Express</a> <strong>:</strong> Back end web application framework for Node.js  </li>
+        </ul>
+        <p><strong>Database</strong></p>
+        <ul>
+        <li><a href="https://www.mongodb.com/">MongoDB</a> <strong>:</strong> A document oriented NoSQL database</li>
         </ul>
     </div>
   )
