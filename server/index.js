5a6014ecb18ab43f0f3247bf3960ae4959f233bd
keyp0s <aidentubman@gmail.com>
2022-03-28 21:59:49 +1100

    initial

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require("express")
 const app = express();
 const dotenv = require("dotenv")
-const authRoute = require("./routes/auth");
-const usersRoute = require("./routes/users");
-const postsRoute = require("./routes/posts");
+const authRoute = require("./routes/auth")
+const usersRoute = require("./routes/users")
+const postsRoute = require("./routes/posts")
 
 dotenv.config()
 app.use(express.json())
@@ -13,11 +13,11 @@ mongoose.connect(process.env.MONGO_URL)
 .then(console.log("Connected to MongoDB"))
 .catch((err) => console.log(err))
 
-app.use("/server/auth", authRoute);
-app.use("/server/users", usersRoute);
-app.use("/server/posts", postsRoute);
+app.use("/server/auth", authRoute)
+app.use("/server/users", usersRoute)
+app.use("/server/posts", postsRoute)
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req,res)=>{
     res.send('hello')
