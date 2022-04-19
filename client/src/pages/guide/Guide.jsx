004295fcb88358fcb2088b9c859ef35fb104e0d8
aiden tubman <aidentubmann@gmail.com>
2022-04-20 09:22:33 +1000

    started work on guide

--- client/src/pages/guide/Guide.jsx
+++ client/src/pages/guide/Guide.jsx
@@ -5,7 +5,7 @@ export default function guide() {
     return (
         <>
             <div className='guide'>
-                <h1>A simple guide to ricing</h1>
+                <h2>A simple guide to ricing</h2>
                 <iframe
                     width='560'
                     height='315'
@@ -15,6 +15,31 @@ export default function guide() {
                     allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
                     allowfullscreen
                 ></iframe>
+                <h3>What is ricing?</h3>
+                <p>
+                    The term 'rice' originated from street racing and means Race
+                    Inspired Cosmetic Enhancements.
+                </p>
+                <p>
+                    It is now commonly used to describe visual customisations to
+                    the linux desktop that dont provide any practical benefits.
+                </p>
+                <h3>How do I start ricing?</h3>
+                <p>
+                    The first step is to choose your distribution. Though you
+                    can really use any distribution many people lean towards
+                    <a className ='guideLink' href='https://archlinux.org/'> Arch Linux</a> because of its high flexibility.
+                </p>
+                <h3>What can I customise?</h3>
+                <ul className='guideList'>
+                    <li className='guideListItem'>
+                        <b>Desktop Environment (DE):</b> A desktop environment is a collection of programs bundled together to make a workspace. It comes with a window manager, a panel, a set of default programs (text editor, music player, etc.), a file manager, a terminal emulator, and many, many other things. Some commonly used are
+                    </li>
+                    <li className='guideListItem'>
+                        <b>Window Manager (WM):</b> A window manager is software for an operating system that manages the placement of open windows. There are many window managers for Linux, most commonly used are
+                    </li>
+
+                </ul>
             </div>
             <Footer />
         </>
